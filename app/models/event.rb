56a379947a66ce0336b347f3376db5e11863650d
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-12-29 23:12:29 +0900

    ADD: チケットのデータベース追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :owner, class_name: "User"
+  has_many :tickets
 
   validates :name, length: { maximum: 50 }, presence: true
   validates :place, length: { maximum: 100 }, presence: true
@@ -8,6 +9,13 @@ class Event < ApplicationRecord
   validates :end_at, presence: true
 
   validate :start_at_should_be_before_end_at
+
+  def created_by?(user)
+    return false unless user
+    owner_id == user.id
+  end
+
+  private
 
   def start_at_should_be_before_end_at
     return unless start_at && end_at
